229c95169e6c26cd63f77739e72d1f624bd55690
anandan.gunasekaran@swordgroup.in <anandan.gunasekaran@swordgroup.in>
2024-04-12 21:33:19 +0530

    counter increment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import FirstCompo from './Components/FirstCompo';
+
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  const handleClick = () => {
+   setCount(count+1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <p>Welcome React!</p>
+   <p>I clicked the button {count} times</p>
+   <button onClick={handleClick}>Check</button>
+   <FirstCompo></FirstCompo>
+   </>
   );
 }
 
---
+++ src/Components/FirstCompo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FirstCompo() {
+
+    return(
+    <>
+    <p>Hello First Compo!!</p>
+    </>
+    )
+}
+export default FirstCompo;

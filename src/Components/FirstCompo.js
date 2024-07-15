2a19bc3a581ce69e2a9c4e6401095e075230cee9
SaiKalpana07 <saireddy7991@gmail.com>
2024-07-15 16:11:27 +0530

    autocomplete using react

--- src/Components/FirstCompo.js
+++ src/Components/FirstCompo.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FirstCompo() {
+    const data = ["apple","mango","berry","avocado","strawberry"]
+    const[inputValue,setInputValue] = useState("")
+    const[suggestions,setSuggestions] = useState([])
+    const handleType = (event) => {
+     let input = event.target.value
+        console.log(input,"input")
+        setInputValue(input)
+        console.log(inputValue,"inputValue")
+        if(input.trim() === ''){
+            setSuggestions([])
+        }
+        else {
+        const filteredSuggestions = data.filter((d)=>{console.log(d,typeof d,typeof input,'d')
+           const correctMatch = d.toLowerCase().includes(input.toLowerCase())
+           return correctMatch
+        })
+    
+        console.log(filteredSuggestions,"suggestions")
+        setSuggestions(filteredSuggestions)
+    }
+}
+
+    const handleClick = () =>{
+     setInputValue("")
+    }
 
     return(
     <>
     <p>Hello First Compo!!</p>
+    <input type="text" value={inputValue} onChange={handleType} />  <button onClick={handleClick}>Reset</button>
+    <ul>
+        {suggestions.length > 0 && suggestions.map((suggestion,index)=>(
+            <li key={index}>{suggestion}</li>
+        ))}
+    </ul>
     </>
     )
 }
